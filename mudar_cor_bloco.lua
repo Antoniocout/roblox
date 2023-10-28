c33ab12be92385dd3e1f847cb846c053d9a6648e
Antônio Coutinho <antoniocoutinhorodriguesalves@gmail.com>
2023-10-28 17:55:30 -0300

    primeiro codigo no github

---
+++ mudar_cor_bloco.lua
@@ -0,0 +1,13 @@
+function comando()
+	game.Workspace.Part.BrickColor = BrickColor.Blue()
+end
+
+
+workspace.bloco2.Touched:Connect(comando)
+
+function comando()
+	game.Workspace.Part.BrickColor = BrickColor.Green()
+end
+
+
+workspace.bloco1.Touched:Connect(comando)
